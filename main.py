650a23e7a594a55d09c57f24c85d0920a3092876
Harry <bars111000@bk.ru>
2021-09-10 00:15:05 +1000

    Change sqlite to postgress

--- main.py
+++ main.py
@@ -56,9 +56,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
 
 # STEP 6. Set a login_manager:
 login_manager = LoginManager()
